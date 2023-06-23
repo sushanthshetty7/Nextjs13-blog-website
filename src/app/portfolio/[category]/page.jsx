4a52f229d3dd75c56f2b1c85fdc6e2cd2db53fc0
sushanthshetty7 <sushanthr052@gmail.com>
2023-06-23 22:43:26 +0530

    fetch static

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -2,49 +2,39 @@ import React from 'react'
 import styles from "./Page.module.css"
 import Button from '@/components/Button/Button'
 import Image from 'next/image'
+import {items} from './data'
+import { PageNotFoundError } from 'next/dist/shared/lib/utils'
+
+const getdata = (cat)=>{
+  const data = items[cat]
+  if(data){
+    return data
+  }
+
+  return PageNotFoundError
+}
 
 function Category({params}) {
-  console.log(params)
+  const data = getdata(params.category)
+
   return (
 
     <div className={styles.catogary}>
       <h1 className={styles.catTitle}>{params.category}</h1>
-      <div className={styles.item}>
+      {data.map(item=>(
+        <div className={styles.item} key={item.id}>
         <div className={styles.textContainer}>
-          <h1>Test</h1>
-          <p>desc</p>
+          <h1>{item.title}</h1>
+          <p>{item.desc}</p>
           <Button text={"see more"} url={""}/>
         </div>
         <div className={styles.imgContainer}>
           <Image
-          className={styles.img} fill={true} src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"/>
+          className={styles.img} fill={true} src={item.image}/>
         </div>
-        
       </div>
-      <div className={styles.item}>
-        <div className={styles.textContainer}>
-          <h1>Test</h1>
-          <p>desc</p>
-          <Button text={"see more"} url={""}/>
-        </div>
-        <div className={styles.imgContainer}>
-          <Image
-          className={styles.img} fill={true} src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"/>
-        </div>
-        
-      </div>
-      <div className={styles.item}>
-        <div className={styles.textContainer}>
-          <h1>Test</h1>
-          <p>desc</p>
-          <Button text={"see more"} url={""}/>
-        </div>
-        <div className={styles.imgContainer}>
-          <Image
-          className={styles.img} fill={true} src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"/>
-        </div>
-        
-      </div>
+      ))}
+      
     </div>
   )
 }                        
